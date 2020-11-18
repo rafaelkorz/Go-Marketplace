a2dd3afe9b39b528fcad1e2d1bd6585d7ac2dc6f
Rafael Korz <rafael.korz@gmail.com>
2020-11-18 14:53:03 -0300

    Finished

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1>GoMarketplace</h1>
+
+## 🚀 Tecnologias
+
+Esse projeto Mobile foi desenvolvido com as seguintes tecnologias:
+
+- [React-Native]
+- [Styled-components]
+
+## 💻 Projeto
+
+Um Projeto simples de um  marketplace.
+  <h4>Features:</h4>
+  <ul>
+    <li>Dashboard de produtos</li>
+    <li>Carrinho de comprar</li>
+    <li>Total de itens no carrinho</li>
+    <li>Total dos produtos no carrinho</li>
+  </ul>
+
+## 🔖 Layout
+
+<table>
+  <tr>
+    <td><strong>Dashboard dos produtos</strong></td>
+    <td><strong>Carrinho</strong></td>
+  <tr>
+   <tr>
+    <td><img src="screen/screen1.png"></td>
+    <td><img src="screen/screen2.png"></td>
+  <tr>
+</table>
+
+## Licença
+
+Esse projeto está sob a licença MIT.
+
+Execute yarn install ou npm install para instalação.
+
+Execute yarn json-server server.json -p 3333 para iniciar o backend.
+
+Execute yarn android ou yarn ios para carregar o App.
---
+++ src/hooks/cart.tsx
@@ -0,0 +1,125 @@
+import React, {
+  createContext,
+  useState,
+  useCallback,
+  useContext,
+  useEffect,
+} from 'react';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContext {
+  products: Product[];
+  addToCart(item: Omit<Product, 'quantity'>): void;
+  increment(id: string): void;
+  decrement(id: string): void;
+}
+
+const CartContext = createContext<CartContext | null>(null);
+
+const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function loadProducts(): Promise<void> {
+      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsStorage = await AsyncStorage.getItem(
+        '@GoMarketplace:produts',
+      );
+      if (productsStorage) {
+        setProducts([...JSON.parse(productsStorage)]);
+      }
+      // await AsyncStorage.multiRemove(['@GoMarketplace:produts']);
+    }
+
+    loadProducts();
+  }, []);
+
+  const addToCart = useCallback(
+    async product => {
+      let newProducts = [];
+      const productExists = products.find(item => item.id === product.id);
+
+      if (productExists) {
+        newProducts = products.map(item =>
+          item.id === product.id
+            ? { ...product, quantity: item.quantity + 1 }
+            : item,
+        );
+      } else {
+        newProducts = [...products, { ...product, quantity: 1 }];
+      }
+
+      setProducts(newProducts);
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:produts',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async id => {
+      const newProducts = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : product,
+      );
+
+      setProducts(newProducts);
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:produts',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const newProducts = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity - 1 }
+          : product,
+      );
+
+      setProducts(newProducts);
+
+      await AsyncStorage.setItem(
+        '@GoMarketplace:produts',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
+
+  const value = React.useMemo(
+    () => ({ addToCart, increment, decrement, products }),
+    [products, addToCart, increment, decrement],
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+function useCart(): CartContext {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error(`useCart must be used within a CartProvider`);
+  }
+
+  return context;
+}
+
+export { CartProvider, useCart };
